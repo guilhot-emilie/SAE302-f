b400d29cb3f5de5bdb0ee5f610cc0f1218b9d3fb
emili <emilie.guilhot@uha.fr>
2022-11-30 16:24:21 +0100

    master

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-import socket
+import socket, threading
 
 class server:
     def __init__(self, host, port):
@@ -6,14 +6,14 @@ class server:
         self.__host = host
         self.__socket = None
 
-    def connected(self):
+    def __connected(self):
         self.__socket = socket.socket()
         self.__socket.connect((self.__host,self.__port))
 
     def isConnected(self):
         return(self.__socket!=None)
 
-    def send(self,msg):
+    def __send(self,msg):
         if self.isConnected():
             self.__socket.send(msg.encode())
             msg=self.__socket.recv(1024).decode()
@@ -23,3 +23,15 @@ class server:
 
     def close(self):
         self.__socket.close()
+
+    def connect(self):
+        self.__tconnected = threading.Thread(target = self.__connect)
+        self.__tconnected.start()
+
+
+    def send(self):
+        self.__tsend = threading.Thread(target = self.__send)
+        self.__tsend.start()
+        self.verrou = threading.Lock()
+        self.verrou.acquire()
+        self.verrou.release()
---
+++ test3.py
@@ -0,0 +1,17 @@
+import socket
+import threading
+from client import server
+
+host1 = input("1er serveur")
+host2 = input("2eme serveur")
+hostsock1 = server(host1, 1001)
+hostsock2 = server(host2, 1001)
+hostsock1.connect()
+hostsock2.connect()
+Thread.sleep(2)
+
+for i in range(10)
+    hostsock1.send("OS")
+    hostsock2.send("CPU")
+hostsock1.close()
+hostsock2.close()
